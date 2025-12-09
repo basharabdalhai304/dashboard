61701e2efe2ae5f53dcac8caef992978d3594512
bashar <basharabdalhai304@gmail.com>
2025-12-09 14:58:14 +0300

    add nivo library and add barchar

--- src/Components/Pages/BarChart/BarChart.jsx
+++ src/Components/Pages/BarChart/BarChart.jsx
@@ -1,7 +1,91 @@
+import { ResponsiveBar } from '@nivo/bar'
 
-export function BarChart(){
-    return(
-        <h3>Bar Chart</h3>
-    )
-}
+const data = [
+    {
+      "country": "AD",
+      "hot dog": 45,
+      "burger": 93,
+      "sandwich": 191,
+      "kebab": 3,
+      "fries": 148,
+      "donut": 144
+    },
+    {
+      "country": "AE",
+      "hot dog": 138,
+      "burger": 198,
+      "sandwich": 168,
+      "kebab": 36,
+      "fries": 170,
+      "donut": 46
+    },
+    {
+      "country": "AF",
+      "hot dog": 59,
+      "burger": 54,
+      "sandwich": 64,
+      "kebab": 75,
+      "fries": 41,
+      "donut": 5
+    },
+    {
+      "country": "AG",
+      "hot dog": 190,
+      "burger": 177,
+      "sandwich": 103,
+      "kebab": 40,
+      "fries": 106,
+      "donut": 120
+    },
+    {
+      "country": "AI",
+      "hot dog": 129,
+      "burger": 37,
+      "sandwich": 40,
+      "kebab": 37,
+      "fries": 161,
+      "donut": 195
+    },
+    {
+      "country": "AL",
+      "hot dog": 68,
+      "burger": 127,
+      "sandwich": 164,
+      "kebab": 0,
+      "fries": 80,
+      "donut": 71
+    },
+    {
+      "country": "AM",
+      "hot dog": 185,
+      "burger": 44,
+      "sandwich": 93,
+      "kebab": 21,
+      "fries": 113,
+      "donut": 46
+    }
+  ]
+export const BarChart = () => (
+    <ResponsiveBar
+    data={data}
+    keys={['hot dog', 'burger', 'sandwich', 'kebab', 'fries', 'donut']} // 👈 ضروري
+    indexBy="country"
+    labelSkipWidth={12}
+    labelSkipHeight={12}
+    legends={[
+        {
+            dataFrom: 'keys',
+            anchor: 'bottom-right',
+            direction: 'column',
+            translateX: 120,
+            itemsSpacing: 3,
+            itemWidth: 100,
+            itemHeight: 16
+        }
+    ]}
+    axisBottom={{ legend: 'country (indexBy)', legendOffset: 32 }}
+    axisLeft={{ legend: 'food', legendOffset: -40 }}
+    margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+/>
 
+)
--- src/Components/Pages/FAQPage/FAQPage.jsx
+++ src/Components/Pages/FAQPage/FAQPage.jsx
@@ -1,7 +1,59 @@
 
-export function FAQPage(){
-    return(
-        <h3>FAQ Page</h3>
-    )
-}
+import Accordion from '@mui/material/Accordion';
+import AccordionActions from '@mui/material/AccordionActions';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Button from '@mui/material/Button';
 
+export function FAQPage() {
+  return (
+    <div style={{ margin: '80px auto', width: '95%' }}>
+        <h3 style={{ textAlign: 'center', marginBottom: '20px',backgroundColor: 'primary.main',color: '#1976d2',padding: '10px',borderRadius: '10px' }}>Frequently Asked Questions</h3>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1-content"
+          id="panel1-header"
+        >
+          <Typography component="span">Accordion 1</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2-content"
+          id="panel2-header"
+        >
+          <Typography component="span">Accordion 2</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        </AccordionDetails>
+      </Accordion>
+      <Accordion defaultExpanded>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3-content"
+          id="panel3-header"
+        >
+          <Typography component="span">Accordion Actions</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        </AccordionDetails>
+        <AccordionActions>
+          <Button>Cancel</Button>
+          <Button>Agree</Button>
+        </AccordionActions>
+      </Accordion>
+    </div>
+  );
+}
